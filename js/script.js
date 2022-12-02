77f8b92198d0d6c9256639bd2d3b889d4fbcbacf
Zuzanna Komendecka <z.komendecka@gmail.com>
2022-12-02 12:46:33 +0100

    Add eslint

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
+/* eslint-disable indent */
 'use strict';
 
-function titleClickHandler(event){
-    event.preventDefault();
-    const clickedElement = this;
+function titleClickHandler(event) {
+  event.preventDefault();
+  const clickedElement = this;
 
     /* [DONE] remove class 'active' from all article links  */
 
     const activeLinks = document.querySelectorAll('.titles a.active');
 
-    for(let activeLink of activeLinks){
+    for (let activeLink of activeLinks) {
         activeLink.classList.remove('active');
     }
     /* [DONE] add class 'active' to the clicked link */
@@ -17,7 +18,7 @@ function titleClickHandler(event){
     /* [DONE] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts .active');
 
-    for(let activeArticle of activeArticles){
+    for (let activeArticle of activeArticles) {
         activeArticle.classList.remove('active');
     }
     /* [DONE] get 'href' attribute from the clicked link */
@@ -38,7 +39,7 @@ const optArticleSelector = '.post',
     optTitleSelector = '.post-title',
     optTitleListSelector = '.titles';
 
-function generateTitleLinks(){
+function generateTitleLinks() {
 
     /* [DONE] remove contents of titleList */
     const titleList = document.querySelector(optTitleListSelector);
@@ -50,7 +51,7 @@ function generateTitleLinks(){
 
     let html = '';
 
-    for (let article of articles){
+    for (let article of articles) {
 
         /* [DONE] get the article id */
         const articleId = article.getAttribute('id');
@@ -68,7 +69,7 @@ function generateTitleLinks(){
 
     titleList.innerHTML = html;
     const links = document.querySelectorAll('.titles a');
-    for(let link of links){
+    for (let link of links) {
         link.addEventListener('click', titleClickHandler);
     }
 }
